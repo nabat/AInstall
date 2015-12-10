57f8a761d47e865c2dbd671d11275abf1ce6c9e8
Horodchuk Anton <antoman1994@gmail.com>
2015-12-10 14:37:57 +0200

    Update alib.sh
    Fixes syntax

--- alib.sh
+++ alib.sh
@@ -105,7 +105,7 @@ guess_plugin(){
 	PLUGIN_OS_ARCH='86';
 	ARCH64=`echo ${MACH} | grep -o -e '64'`;
  	if [ ${ARCH64} ]; then
-	    PLUGIN_OS_ARCH = '64';
+	    PLUGIN_OS_ARCH='64';
 	fi
 
 	PLUGIN_NAME="${PLUGIN_OS_NAME}_${PLUGIN_OS_VERSION}_x${PLUGIN_OS_ARCH}";
